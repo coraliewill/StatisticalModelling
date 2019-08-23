e30b633345dbbf33ec86898152ac05bfc9660dbd
Coralie Williams <coraliewilliams@outlook.com>
2019-08-23 09:36:45 +0200

    Update GLMS_example.R

--- GLMS_example.R
+++ GLMS_example.R
@@ -3,11 +3,14 @@
 set.seed(1234)
 seq(from=0, to=5, by=0.01)
 
-model_poly <- lm(y ~ poly(x1) + poly(x2)
+model_poly <- lm(y ~ poly(x1) + poly(x2))
   
 
 ## Binomial GLM
+model_binom <- glm(y ~ x1 + x2 + x3, family="binomial")            
   
 ## Gaussian GLM
+model_binom <- glm(y ~ x1 + x2 + x3, family="gaussian")      
 
 ## Poisson GLM
+model_binom <- glm(y ~ x1 + x2 + x3, family="poisson")      
